4213581a778e6de234850aa0020a5ef212b729dc
vikaashpanchbikesan <117252370+vikaashpanchbikesan@users.noreply.github.com>
2023-04-17 19:14:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+music1 = "";
+music2 = "";
+
+function preload(){
+        music1 = loadsound("music.mp3");
+        music2 = loadsound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
